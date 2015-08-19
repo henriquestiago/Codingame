c39eb82fee3b2ee690189cb53195d5b06dce7138
henriquestiago <th.tk@hotmail.com>
2015-08-19 23:24:17 +0100

    Create HorseRacingDuals.bash

---
+++ HorseRacingDuals.bash
@@ -0,0 +1,25 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+read n
+declare -a horses
+declare -a sorted
+lower=10000000; last=-1
+
+for((i=0 ; i<n ; i++)); do
+    read horse
+    sorted[horse]=1
+done
+
+if [[ ${#sorted[@]} -ne n ]]; then
+    lower=0;
+else
+    for i in ${!sorted[@]}; do
+        if [[ $last -ne -1 && $(($i-$last)) -lt $lower ]]; then 
+            lower=$(($i-$last));
+        fi
+        last=$i
+    done
+fi
+
+echo $lower
